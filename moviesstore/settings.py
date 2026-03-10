11b06100b9910b7cfe49443edffc5ae6197d1e83
Exahel Castaneda <exahelcastaneda@gmail.com>
2026-03-10 19:45:18 -0400

    Nevermind

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -21,18 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+SECRET_KEY = 'django-insecure-ccv#$qf(is7oac1#^%m!jen%-m^scnqiji&@p*xy*fmy8l2pwl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    "ExahelC.pythonanywhere.com",
-    "localhost",
-    "127.0.0.1"
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+ALLOWED_HOSTS = []
 
 
 # Application definition
